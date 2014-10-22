d9d9576c08d9b9ccb19d92f2e5e829a37037bc50
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:48:21 -0500

    Clean up before final

--- src/UrlFieldType.php
+++ src/UrlFieldType.php
@@ -4,29 +4,12 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class UrlFieldType extends FieldTypeAddon
 {
-    protected $slug = 'url';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'string';
 
-    /**
-     * Initial field type validation requirements.
-     *
-     * @var array
-     */
     public $validation = array(
         'url'
     );
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
     public function input()
     {
         return \Form::input('text', $this->inputName(), $this->value);
