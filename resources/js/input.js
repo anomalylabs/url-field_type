ed093cb6c46f8fb3ac845e6aec7b3234bb8c55a4
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-14 02:22:31 +0300

    -> jQuery

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,33 +1,37 @@
-+(function (window, document) {
-  document.addEventListener('DOMContentLoaded', function () {
+$(function () {
     var
-      inputs = document.querySelectorAll('[data-provides="anomaly.field_type.url"]'),
-      scheme = window.location.protocol + '//';
+        $inputs = $('[data-provides="anomaly.field_type.url"]'),
+        scheme = window.location.protocol + '//';
 
-    inputs.forEach(function (input) {
-      input.placeholder = scheme;
+    $inputs.attr('placeholder', scheme);
 
-      input.addEventListener('keyup', function (ev) {
-        if (ev.target.value) {
-          if (!ev.target.value.match(/^https?:\/\//)) {
-            ev.target.value = scheme + ev.target.value;
-          }
+    $inputs.on('keyup', function (event) {
+        var
+            $target = $(event.target),
+            value = $target.val();
+
+        if (value && !value.match(/^https?:\/\//)) {
+            $target.val(scheme + value);
         }
-      });
-
-      input.addEventListener('blur', function (ev) {
-        if (ev.target.value) {
-          if (['http://', 'https://'].includes(ev.target.value)) {
-            ev.target.value = '';
-          }
-        }
-      });
-
-      input.addEventListener('focus', function (ev) {
-        if (!ev.target.value) {
-          ev.target.value = scheme;
-        }
-      });
     });
-  });
-})(window, document);
+
+    $inputs.on('blur', function (event) {
+        var
+            $target = $(event.target),
+            value = $target.val();
+
+        if (value && ['http://', 'https://'].includes(value)) {
+            $target.val('');
+        }
+    });
+
+    $inputs.on('focus', function (event) {
+        var
+            $target = $(event.target),
+            value = $target.val();
+
+        if (!value) {
+            $target.val(scheme);
+        }
+    });
+});
