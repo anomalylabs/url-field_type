2a3b029bf2b5004234cc09713cf481918db57e7c
Aidan Watt <aidan@hotfootdesign.co.uk>
2025-10-19 13:22:09 +0100

    Revert for new branch

--- src/UrlFieldTypePresenter.php
+++ src/UrlFieldTypePresenter.php
@@ -1,7 +1,7 @@
 <?php namespace Anomaly\UrlFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
-use Anomaly\Streams\Platform\Html\HtmlBuilder;
+use Collective\Html\HtmlBuilder;
 
 /**
  * Class UrlFieldTypePresenter
